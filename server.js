4e8ae485362d88d1e6c01b5395697d3757679c84
Zazil Ibarra <zaziil.97@gmail.com>
2021-01-11 14:05:23 -0600

    Añade dependenciasn funcionales

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const router = require('./src/routes/index');
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(require('./routes/index'));
+app.use('/', router);
 
 app.listen(app.get('port'), () => {
     console.log("Server en el puerto: ", app.get('port'))
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,40 @@
-const {Router} = require('express');
-const router = new Router();
+const express = require('express');
+const router = express.Router();
+const app = express();
+const matrix = require('matrix-js');
+var fs = require("fs"), PNG = require("pngjs").PNG;
 
-router.get("/", (req, res) =>{
-    res.render("home");
+app.set('view engine', 'ejs');
+
+router.get("/", (req, res) => {
+    var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    var A = matrix(a);
+
+    fs.createReadStream("./src/public/img/images.png")
+        .pipe(
+            new PNG({
+                filterType: 4,
+            })
+        )
+        .on("parsed", function () {
+            for (var y = 0; y < this.height; y++) {
+                for (var x = 0; x < this.width; x++) {
+                    var idx = (this.width * y + x) << 2;
+
+                    // invert color
+                    this.data[idx] = 255 - this.data[idx];
+                    this.data[idx + 1] = 255 - this.data[idx + 1];
+                    this.data[idx + 2] = 255 - this.data[idx + 2];
+
+                    // and reduce opacity
+                    this.data[idx + 3] = this.data[idx + 3] >> 1;
+                }
+            }
+
+            this.pack().pipe(fs.createWriteStream("out.png"));
+        });
+
+    res.render("home", { data: a });
 });
 
 module.exports = router;
